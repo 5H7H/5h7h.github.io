76bba616db2ca26f9f8a96f87af278e6103a73ba
lan <lj350523270@gmail.com>
2026-05-18 21:24:28 +0900

    fix: 修复 GitHub Pages 的白屏

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './common.css';
 import './post_style.css';
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // 将绝对路径改为相对路径，防止 GitHub Pages 找不到静态资源文件
 })
